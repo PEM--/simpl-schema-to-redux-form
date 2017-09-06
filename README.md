3d4a330fad125b7ed46a1d2491d5fcc3178a5d04
PEM-- <pemarchandet@gmail.com>
2017-09-06 10:40:29 +0200

    Logging capability & docs

--- README.md
+++ README.md
@@ -6,3 +6,28 @@
 [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
 SimpleSchema to Redux Form
+
+## Create a validation context
+```
+import SimpleSchema from 'simpl-schema'
+import { simpleSchemaToReduxFormValidator } from 'simpl-schema-to-redux-form'
+
+// Create your own schema
+const schema = new SimpleSchema({ ... })
+
+// Create a validation context for this schema
+const ctx = mainSchema().newContext()
+
+// Create your validate function for redux-form
+const validate = simpleSchemaToReduxFormValidator(ctx)
+```
+
+## Current values and validation state
+The created `validate` function exposes 2 members: `values`, `errors`.
+
+## Add logger
+In development, you can log each call to the validation function by passing a logger.
+
+```
+const validate = simpleSchemaToReduxFormValidator(ctx, console.log)
+```
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import dot from 'dot-object'
 
-export const simpleSchemaToReduxFormValidator = (schema, ctx) => (values) => {
-  ctx.validate(values)
-  if (ctx.isValid()) return {}
-  const ssErrors = ctx.validationErrors()
-  const rfRes = ssErrors.reduce((acc, ssError) => {
-    // Avoid taking into account multiple errors on one field, takes only the first
-    if (acc[ssError.name]) return acc
-    const ssMsg = ctx.keyErrorMessage(ssError.name)
-    return { ...acc, [ssError.name]: ssMsg }
-  }, {})
-  return dot.object(rfRes)
+export const simpleSchemaToReduxFormValidator = (ctx, logger) => {
+  const func = (values) => {
+    ctx.validate(values)
+    if (ctx.isValid()) return {}
+    const ssErrors = ctx.validationErrors()
+    const rfRes = ssErrors.reduce((acc, ssError) => {
+      // Avoid taking into account multiple errors on one field, takes only the first
+      if (acc[ssError.name]) return acc
+      const ssMsg = ctx.keyErrorMessage(ssError.name)
+      return { ...acc, [ssError.name]: ssMsg }
+    }, {})
+    func.values = values
+    func.errors = rfRes
+    if (logger) {
+      logger.log('simpleSchemaToReduxFormValidator', { values: func.values, errors: func.errors })
+    }
+    return dot.object(rfRes)
+  }
+  func.values = null
+  func.errors = null
+  return func
 }
