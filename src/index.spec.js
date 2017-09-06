9cde468590b2ba7d4362ece9938094ebea672685
PEM-- <pemarchandet@gmail.com>
2017-09-06 10:48:49 +0200

    Logging capability & docs

--- src/index.spec.js
+++ src/index.spec.js
@@ -23,15 +23,16 @@ const schema = new SimpleSchema({
 })
 
 const ctx = schema.newContext()
-const validator = simpleSchemaToReduxFormValidator(schema, ctx)
-const res = validator({
+const validate = simpleSchemaToReduxFormValidator(ctx)
+const values = {
   testString: 'Test',
   testObject: { string: '', number: 0 },
   testArray: [{ string: '', number: 0 }, { string: '' }]
-})
+}
+const res = validate(values)
 
-it('should succeed', () => {
-  expect(res).toEqual({
+it('should detect errors', () => {
+  const expectedErrors = {
     testArray: [
       undefined,
       {
@@ -39,5 +40,8 @@ it('should succeed', () => {
       }
     ],
     testNumber: { id: 'forms.required' }
-  })
+  }
+  expect(res).toEqual(expectedErrors)
+  expect(validate.values).toEqual(values)
+  expect(validate.errors).toEqual(expectedErrors)
 })
