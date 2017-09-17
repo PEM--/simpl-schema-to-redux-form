465981a1c23123eab1b797d97523b993b5cb6561
PEM-- <pemarchandet@gmail.com>
2017-09-17 16:07:59 +0200

    Autoform - mainSchema creation with custom transform

--- src/modules/autoForm.js
+++ src/modules/autoForm.js
@@ -1,18 +1,32 @@
 import moize from 'moize'
 import React from 'react'
+// import { compose, onlyUpdateForKeys, pure, withHandlers, withPropsOnChange } from 'recompose'
+// import { Field, reduxForm } from 'redux-form'
+import SimpleSchema from 'simpl-schema'
 
 class Autoform {
   constructor (options) {
     this.options = {
       defaultComponent: React.DOM.input,
+      modelFieldTransformer: this.defaultModelFieldTransformer,
       ...options
     }
     this.formFields = this.options.formFields
+    this.model = this.options.model
+    this.modelFieldTransformer = this.options.modelFieldTransformer
   }
+  defaultModelFieldTransformer = (model, fieldName) =>
+    ({ ...model[`${fieldName}.current`], optional: true })
   fieldGroups = moize(() => Object.keys(this.formFields), { maxArgs: 0 })
   modelKeys = moize(() =>
     this.fieldGroups().reduce((acc, key) =>
       acc.concat(Object.keys(this.formFields[key].fieldGroup)),
     []), { maxArgs: 0 })
+  mainSchema = moize(() => new SimpleSchema(
+    this.modelKeys().reduce((acc, key) => {
+      acc[key] = this.modelFieldTransformer(this.model, key)
+      return acc
+    }, {})
+  ), { maxArgs: 0 })
 }
 export default Autoform
--- src/modules/autoForm.spec.js
+++ src/modules/autoForm.spec.js
@@ -1,6 +1,21 @@
+import React from 'react'
+import omit from 'lodash/omit'
+import SimpleSchema from 'simpl-schema'
+
 import { Autoform } from '../../index.min'
 
-const autoform = new Autoform({
+const config = {
+  formName: 'EditForm',
+  model: {
+    description: { type: String },
+    location: { type: String },
+    comments: { type: String },
+    // This field is not used in the form
+    otherKeys: { type: String }
+  },
+  modelFieldTransformer: (model, fieldName) => ({ ...model[fieldName] }),
+  ValidationButton: React.DOM.button,
+  CancellationButton: React.DOM.button,
   formFields: {
     firstLine: {
       fieldGroup: {
@@ -14,8 +29,20 @@ const autoform = new Autoform({
       }
     }
   }
+}
+
+const autoform = new Autoform(config)
+
+it('produces a list of fieldGroups', () => {
+  expect(autoform.fieldGroups()).toEqual(['firstLine', 'secondLine'])
 })
 
-it('should produce a list of distinct keys on the model', () => {
+it('produces a list of distinct keys on the model', () => {
   expect(autoform.modelKeys()).toEqual(['description', 'location', 'comments'])
+})
+
+it('produces a SimpleSchema for the form', () => {
+  const resultSchema = new SimpleSchema(omit(config.model, 'otherKeys'))
+  expect(autoform.mainSchema().objectKeys()).toEqual(resultSchema.objectKeys())
+  expect(autoform.mainSchema().objectKeys()).toEqual(autoform.modelKeys())
 })
